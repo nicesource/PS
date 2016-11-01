2dab4e58430bc72b9fa7bc314817898120cb6338
nicesource <sourceeme@gmail.com>
2016-11-01 21:28:06 +0330

    Create rmsg.lua

---
+++ plugins/rmsg.lua
@@ -0,0 +1,35 @@
+local function history(extra, suc, result)
+  for i=1, #result do
+    delete_msg(result[i].id, ok_cb, false)
+  end
+  if tonumber(extra.con) == #result then
+    send_msg(extra.chatid, '"'..#result..'" پیام اخیر سوپر گروه حذف شد', ok_cb, false)
+  else
+    send_msg(extra.chatid, 'تعداد پیام مورد نظر شما پاک شد', ok_cb, false)
+  end
+end
+local function run(msg, matches)
+  if matches[1] == 'rmsg' and is_owner(msg) then
+    if msg.to.type == 'channel' then
+	if redis:get("id:"..msg.to.id..":"..msg.from.id) then
+return "⚜ این دستور هر نیم ساعت امکان پذیر است\n نیم ساعت شما هنوز تموم نشده است، لطفا دوباره امتحان نکنید"
+end
+redis:setex("id:"..msg.to.id..":"..msg.from.id, 60, true)
+      if tonumber(matches[2]) > 500 or tonumber(matches[2]) < 1 then
+        return "تعداد بیشتر از 500 مجاز نیست"
+      end
+      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+    else
+      return "فقط در سوپرگروه ممکن است"
+    end
+  else
+    return "شما دسترسی ندارید"
+  end
+end
+
+return {
+    patterns = {
+        '^[!/#](del) (%d*)$'
+    },
+    run = run
+}
